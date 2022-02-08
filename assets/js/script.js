df6545ef9880b6f33dc6d79c596caa2f0a87ddbd
Mark8495 <markbyrne8495@gmail.com>
2022-02-08 15:20:55 +0000

    fixed stuff

--- assets/js/script.js
+++ assets/js/script.js
@@ -131,3 +131,4 @@ function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "x";
 }
+
